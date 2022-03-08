d8898222fab00839ea3c0bfd2396016720e4cd9c
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-03-08 00:32:24 -0800

    Time: 252 ms (38.21%), Space: 368.6 MB (16.27%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,19 +1,11 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
-var strStr = function(haystack, needle) {
-    
-   // console.log(haystack.substring(i,needle.length))
-    if(needle.length == 0){
-        return 0;
-    }
-    for(var i = 0; i<haystack.length;i++){
-        if(haystack.substring(i,needle.length+i) == needle){
-            return i;
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size()==0) return 0;
+        for(int i=0,j=needle.size()-1;j<haystack.size();i++,j++){
+            if(haystack.substr(i,needle.size())==needle)
+                return i;
         }
-       
+        return -1;
     }
-     return -1;
 };
