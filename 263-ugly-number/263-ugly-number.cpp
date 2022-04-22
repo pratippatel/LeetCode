62d0ab504da72ad58f6baa6e94c3979d734c4576
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-04-22 13:36:23 -0700

    Time: 3 ms (52.55%), Space: 5.8 MB (69.55%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
 public:
     bool isUgly(int n) {
         
@@ -25,16 +25,4 @@ public:
         return true;
     }
     
-};*/
-class Solution {
-public:
-    bool isUgly(int n) 
-    {
-        if(n<=0) return false;
-        while(n%2 == 0) n /= 2;
-        while(n%3 == 0) n /= 3;
-        while(n%5 == 0) n /= 5;     
-        if(n == 1) return true;
-        return false;
-    }
 };
