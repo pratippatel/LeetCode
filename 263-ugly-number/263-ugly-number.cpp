97397a283a4b992a03e23109ff1bebe803d6b3e1
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-04-22 13:26:08 -0700

    Time: 6 ms (16.12%), Space: 5.9 MB (21.87%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        
+        if(n<=0){
+            return false;
+        }
+        while(n>1){
+            if(n%2 == 0 ){
+                n = n/2;
+                continue;
+            }
+            else if( n%3 == 0){
+                n = n/3;
+                continue;
+            }
+            else if(n%5 == 0){
+                n = n/5;
+                continue;
+            }
+            else if(n%2 != 0 && n%3 != 0  && n%5 != 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+};
