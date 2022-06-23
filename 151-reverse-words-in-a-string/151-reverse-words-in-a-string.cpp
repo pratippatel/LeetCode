94673c92d2c2024191525a4955f26fa8f71245d9
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-06-22 17:04:28 -0700

    Time: 17 ms (25.57%), Space: 7.3 MB (68.90%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
+    
+    string reverse(string s){
+        int start = 0;
+        int end = s.length()-1;
+        
+        while(start< end){
+            swap(s[start], s[end]);
+            start++;
+            end--;
+        }
+        return s;
+    }
+    
     string reverseWords(string s) {
         
-        stack<string> reverse;
+       /* stack<string> reverse;
         string word;
         
         for(int i=0;i<s.length();i++){
@@ -25,6 +38,27 @@ public:
             }
             reverse.pop();
         }
+        return word;*/
+        string word = "";
+        string newword = "";
+        for(int i = 0;i<s.length();i++){
+             if(s[i] == ' '){
+                continue;
+            }
+            while(s[i] !=' ' && i<s.length()){
+                word+= s[i];
+                i++;
+            }
+            newword += reverse(word);
+            newword += " ";
+            word = "";
+            cout<<newword<<endl;
+        }
+        
+        for(int i =newword.size()-2;i>=0;i--){
+            word += newword[i];
+        }
         return word;
+        
     }
 };
