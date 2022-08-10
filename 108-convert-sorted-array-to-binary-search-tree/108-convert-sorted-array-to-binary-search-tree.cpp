ab6d1ee9a2301f781d3e3d7c34d79e0e9c75ce56
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-08-10 16:44:24 -0700

    Time: 15 ms (86.61%), Space: 21.3 MB (77.25%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,37 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    TreeNode* solve(vector<int>& nums,int start,int end){
-        if(start<=end){
-            int mid = start + (end-start) /2;
-
-            TreeNode* root = new TreeNode(nums[mid]);
-
-            root->left = solve(nums, start, mid-1);
-            root->right = solve(nums, mid+1, end);
-
-            return root;    
-        }
-        return NULL;
-    }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size()-1);
+    }
+    
+    TreeNode* helper(vector<int>& nums, int left, int right){
         
-        if(!nums.size()){
+        //base case
+        //If the left pointer crosses right return null;
+        if(left > right){
             return NULL;
         }
-        int start = 0;
-        int end = nums.size()-1;
-        return solve(nums, start, end);
+        
+        // as middle of the array will be the root node
+        int mid = (left + right) / 2;
+        TreeNode* temp = new TreeNode(nums[mid]);
+        
+        //left part from middle will be left subtree
+        temp->left = helper(nums, left, mid-1);
+        
+        //right part of array will be right subtree
+        temp->right = helper(nums, mid+1 , right);
+        return temp;
     }
 };
