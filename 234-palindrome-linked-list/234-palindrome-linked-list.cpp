dde531b4970b0a5d563a41e51dfcab638f231ea2
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-08-23 14:55:04 -0700

    Time: 336 ms (61.61%), Space: 117.9 MB (59.47%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow=head,*fast=head;
+        if(!head->next)  return true;
+        while(fast->next && fast->next->next){              // finding middle of linked list 
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode*prev=NULL,*curr=slow->next,*temp;
+        while(curr!=NULL){                                 // reversing the linked list after the middle node
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        slow->next=prev;                                   // joining the reversed linked list after the middle node
+        ListNode *start=head,*mid=slow->next;
+        while(mid){                                        // valued being checked here
+            if(start->val!=mid->val) return false;
+            start=start->next;
+            mid=mid->next;
+        }
+        return true;
+    }
+};
