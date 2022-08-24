022f5f719c5c797fa9da9bfc0bf1a492c16e97f0
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-08-24 14:29:30 -0700

    Time: 4 ms (40.16%), Space: 6.1 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -4,7 +4,7 @@ public:
     
     bool isPowerOfFour(int n) {
         
-       if(n == 1){
+     /*  if(n == 1){
            return true;
        }
        while(n!=4 && n>4){
@@ -16,9 +16,21 @@ public:
              return false;
          }
        }
-        if(n == 4 ){
+        if(n == 4){
+            return true;
+        }
+        return false;*/
+        
+        //4^x == n , x = log(n)/log(4) is a whole number, %1 ==0 for whole number
+        
+        //cout<<typeid((log(n)/log(4))).name()<<endl;
+        double val = (log(n)/log(4));
+      
+        if(val == floor(val) && val != -1.0 / 0.0){
             return true;
         }
         return false;
+      
+      
     }
 };
