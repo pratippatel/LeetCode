35f1fdf7943d9c6ddf9f1a7c7cde7bd663e94726
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-08-26 17:23:20 -0700

    Time: 9 ms (58.33%), Space: 11.1 MB (8.33%) - LeetHub

--- 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.cpp
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int minNumberOfHours(int initialEnergy, int initialExperience, vector<int>& energy, vector<int>& experience) {
         int hrs = 0;
+        int flag;
+        int extra;
         for(int i=0;i< energy.size();i++){
             if(initialEnergy> energy[i] && initialExperience> experience[i]){
                 initialEnergy = initialEnergy- energy[i];
                 initialExperience = initialExperience + experience[i];
             }
             else if(initialEnergy <= energy[i]){
-                int flag = energy[i] - initialEnergy +1;
+                flag = energy[i] - initialEnergy +1;
                 initialEnergy = initialEnergy + flag;
                 hrs = hrs + flag;
                 i--;
             }
             else if(initialExperience <= experience[i]){
-                int extra = experience[i] - initialExperience +1;
+                extra = experience[i] - initialExperience +1;
                 initialExperience = initialExperience + extra;
                 hrs = hrs + extra;
                 i--;
