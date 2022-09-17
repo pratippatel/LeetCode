eabb5ca376c92f421d29341d3e22ffb3433eb8ad
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-09-16 18:06:56 -0700

    Time: 145 ms (5.01%), Space: 283.2 MB (5.01%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    void solve(string s, string t, int i, int j,int& count){
+       
+        if(i==t.length()){
+            return;
+        }
+        else if(s[j] == t[i]){
+            count++;
+            j= j+1;
+           // solve(s, t,i+1,j+1, count);
+        }
+        
+        solve(s, t,i+1,j, count);
+        
+    }
+    
+    bool isSubsequence(string s, string t) {
+        
+        int count = 0;
+        
+        int i = 0;
+        int j = 0;
+        
+        solve(s, t, i,j, count);
+        
+        return s.length() == count;
+    }
+};
