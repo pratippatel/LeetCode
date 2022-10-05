cd73727a582f541a9026fa0626004da7a4ec632d
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-10-05 13:28:29 -0700

    Time: 35 ms (49.77%), Space: 24.9 MB (87.05%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+/*class Solution {
     
     void solve(TreeNode* root, int val, int depth){
         if(root == NULL){
@@ -39,6 +39,50 @@ public:
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
         
           if(depth == 1){
+            TreeNode* temp = new TreeNode(val);
+            temp->left = root;
+            return temp;
+        }
+        int curr = 1;
+        depth = depth -1;
+        solve(root, val, depth);
+        return root;
+    }
+};
+
+*/
+class Solution {
+    
+    void solve(TreeNode* root, int val, int depth){
+        if(root == NULL){
+            return;
+        }
+        depth = depth -1;
+        if(depth==0){
+            TreeNode* temp = root->left;
+            TreeNode* flag = root->right;
+            
+            root->left = new TreeNode(val);
+            root->right = new TreeNode(val);
+            
+            root->left->left = temp;
+            root->right->right = flag;
+            return;
+        }
+        
+        if(root->left){
+            solve(root->left, val, depth);
+        }
+        if(root->right){
+            solve(root->right, val, depth);
+        }
+        
+    }
+    
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        
+        if(depth == 1){
             TreeNode* temp = new TreeNode(val);
             temp->left = root;
             return temp;
