76abb0c5daa9c377ef64f02e3b58fdf0ebf1a93e
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-10-11 19:01:43 -0700

    Time: 48 ms (77.88%), Space: 22 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -5,20 +5,13 @@ public:
         sort(nums.begin(), nums.end());
         
         int n = nums.size();
-        int sum = 0;
-        int maxPeri =0;
+        
         for(int i = nums.size();i>2;i--){
-            
-            if(nums[i-2] + nums[i-3] > nums[i-1] && 
-               nums[i-1] + nums[i-2] > nums[i-3] && 
-               nums[i-3] + nums[i-1] > nums[i-2]){
-                
-                sum = nums[i-1] + nums[i-2] + nums[i-3];
-                maxPeri = max(maxPeri, sum);
+            if(nums[i-2] + nums[i-3] > nums[i-1] ){
+                return nums[i-1] + nums[i-2] + nums[i-3];
             }
-            
         }
         
-        return maxPeri;
+        return 0;
     }
 };
