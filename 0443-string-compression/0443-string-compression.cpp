a4932fdd1c93f81e99c87c252730e3f5ee799cfa
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-10-15 10:18:19 -0700

    Time: 10 ms (54.03%), Space: 18.1 MB (6.18%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -3,12 +3,10 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         
-        
         int i =0;
         int j =1;
         string s ="";
         while(j<chars.size()){
-            
             if(chars[i] == chars[j]){
                 j++;
             }
@@ -34,18 +32,10 @@ public:
         else{
             s = s + chars[i];
         }
-        cout<<s<<endl;
-        
-       
-        
         for(int i=0;i<s.length();i++){
-            
             chars[i] = s[i];
-            
         }
        
         return s.length();
-        
-        
     }
 };
