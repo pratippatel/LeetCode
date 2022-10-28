ac568f8031f1e9c189d8d5cb139a9e7e55e14359
Pratip Patel <60420780+pratippatel@users.noreply.github.com>
2022-10-28 12:03:01 -0700

    Time: 36 ms (95.01%), Space: 20.6 MB (56.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        unordered_map<string, list<int>> freq;
+        unordered_map<string, vector<string>> freq;
         
         for(int i =0;i< strs.size();i++){
             
@@ -11,22 +11,17 @@ public:
             sort(each.begin(), each.end());
             
             if(freq.find(each) != freq.end()){
-                freq[each].push_back(i);
+                freq[each].push_back(strs[i]);
             }
             else{
-                freq[each].push_back(i);
+                freq[each].push_back(strs[i]);
             }
         }
         
-       
         vector<vector<string>> result;
         
         for(auto each: freq){
-             vector<string> ans;
-            for(auto sep : each.second){
-                ans.push_back(strs[sep]);
-            }
-            result.push_back(ans);
+            result.push_back(each.second);
         }
         return result;
     }
